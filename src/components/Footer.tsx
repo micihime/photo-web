5c134300041c735e3a91bc44790abe4ac6665f08
Miroslava Hreško <miroslava.hresko@gmail.com>
2026-02-27 12:59:02 +0100

    Updating footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,20 @@ import {
   IconMail,
 } from "@tabler/icons-react";
 
-function Footer() {return (
+function Footer() {
+  const handleEmailClick = () => {
+    const username = "mitchie.arte";
+    const domain = "gmail.com";
+    window.location.href = `mailto:${username}@${domain}`;
+  };
+
+  return (
     <div className={styles.footer}>
       <div>
         <a href="https://instagram.com/topinkastudio"><IconBrandInstagram size={30} /></a>
-        <a href="https://instagram.com/topinkastudio"><IconMail size={30} /></a>
+        <button type="button" className={styles.iconButton} onClick={handleEmailClick}>
+          <IconMail size={30} />
+        </button>
       </div>
       <p>Designed by <a href="https://mitchie.dev/">Miči</a></p>
     </div>
