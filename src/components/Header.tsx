a1e02d64308ace81bc2603a46e3180699dfc26b1
Miroslava Hreško <miroslava.hresko@gmail.com>
2026-03-02 14:11:22 +0100

    Slogan update - v1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className={styles.header}>
       <img src={logo} alt="logo" className={styles.logo} />
-      <h3 className={styles.slogan}>short slogan</h3>
+      <h3 className={styles.slogan}>Details Tell the Story.</h3>
     </div>
   )
 }
